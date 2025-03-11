536982868ea6d30f98261ad6fcb08bd41b5e48bd
redmoongames <redmoon.thebest@gmail.com>
2025-03-11 22:51:55 +0300

    hotfix

--- src/components/main/MainPage.tsx
+++ src/components/main/MainPage.tsx
@@ -1,4 +1,4 @@
-import { Button } from '../../ui/Button';
+import { Button } from '../ui/Button';
 import { LoginSkeleton } from './LoginSkeleton';
 import { StickyNote } from './StickyNote';
 import { useLoginContent } from './useLoginContent';
