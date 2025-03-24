d95292b2d20fc95267f9730591191483be9b6089
redmoongames <redmoon.thebest@gmail.com>
2025-03-24 18:47:18 +0300

    test

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -2,5 +2,6 @@ import { NextRequest, NextResponse } from 'next/server';
 import { forwardToBackend } from '../../ProxyUtils';
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
+  console.log('[login] Request received');
   return forwardToBackend(request, '/api/v1/auth/login/');
 } 
